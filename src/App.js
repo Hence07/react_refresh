fd94051da79a63f2d6cf70026a7a21f2b6d89356
Prudence Codjia <pcodjia@ironguard.net>
2020-03-24 16:48:00 -0400

    passimg props, state with function components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Tweet from './components/tweet';
 
 
 
 function App() {
+  const [users, setUsers] = useState([
+    {name: "Eden", message: "Hello there"},
+    {name: "Paul", message: "I am Paul"},
+    {name: "Hill", message: "Bingo!"}
+  ]);
+  
+    
   return(
     <div className= "app">
-  
-      <Tweet name="John"  message="This a random tweet"/>
-      <Tweet name="Tony"  message="I am a true king"/>
-      <Tweet name="Ray"  message="700 k for real tho!"/>
-      <Tweet name="Bond"  message="I love it, come an check thi!"/>
-     
-      
-       
-    </div>
+      {users.map(user => (<Tweet name={user.name} message={user.message} />
+      ))}
+
+  </div>
   )
 }
 
