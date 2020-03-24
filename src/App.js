a4ab8c83ffde3d942b3da43720704b157b35bb5b
Prudence Codjia <pcodjia@ironguard.net>
2020-03-23 23:01:45 -0400

    todo list

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+class App extends React.Component{
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      item_input: ' ',
+      items: [ ]
+
+    };
+    this.update = this.update.bind(this);
+    this.add = this.add.bind(this);
+  }
+  update(event){
+    this.setState({
+      item_input: event.target.value
+    });
+  }
+  add(){
+    this.setState(prev =>{
+      return{
+        item_input: ' ',
+        items: prev.items.concat(prev.item_input)
+      };
+    });
+
+  }
+
+render() {
+  return(
+    <div>
+      <h4>todo list</h4>
+      <ul>
+      {
+         this.state.items.map((item, i) =>(
+         <li>{item}</li>
+        ) )
+      }
+      </ul>
+      <input type="text" value={this.state.item_input}  onChange= {this.update} />
+      <button type="button" onClick={this.add}>add item</button>
+      </div>
   );
+}
 }
 
 export default App;
