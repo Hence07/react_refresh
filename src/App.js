b892c477b8854f226378c38a76346db66d20e5fd
Prudence Codjia <pcodjia@ironguard.net>
2020-03-24 16:02:07 -0400

    Use of function components

--- src/App.js
+++ src/App.js
@@ -1,46 +1,22 @@
 import React from 'react';
 import './App.css';
-import Profiles from './components/profiles'
-import Name from './components/name';
+import Tweet from './components/tweet';
 
 
-class App extends React.Component{
 
-  constructor(props) {
-    super(props);
-    this.state ={
-      top_profiles: [
-        { name: 'Pit', age: 26, bio: 'is a mechanic'},
-        { name: 'Dan', age: 36, bio: 'is a  musician'},
-        { name: 'Peter', age: 33, bio: 'is a preacher'},
-      ],
-      other_profiles: [
-        { name: 'Profile 1', age: 33, bio: 'is a prof 1' },
-        { name: 'Profile 2', age: 33, bio: 'is a prof 2' },
-        { name: 'Profile 3', age: 33, bio: 'is a prof 3' },
-        { name: 'Profile 4', age: 33, bio: 'is a prof 4' },
-        { name: 'Profile 5', age: 33, bio: 'is a prof 5' },
-      ]
-    };
-
-  }
-    render(){
-      return(
-  <div>
-    <h3>Top Profiles</h3>
-    {this.state.top_profiles.map(profile => <Profiles profile={profile} />)}
-
-      <h3> Other Profiles</h3>
-          {this.state.other_profiles.map(profile => <Profiles profile={profile} />)}
-          
-    
+function App() {
+  return(
+    <div className= "app">
+  
+      <Tweet name="John"  message="This a random tweet"/>
+      <Tweet name="Tony"  message="I am a true king"/>
+      <Tweet name="Ray"  message="700 k for real tho!"/>
+      <Tweet name="Bond"  message="I love it, come an check thi!"/>
+     
       
-
-  </div>
-      )
-    }
+       
+    </div>
+  )
 }
 
 export default App;
-
-
